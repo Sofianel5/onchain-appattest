15b4f22f3923d9f2faa8e44c963d0202adc598dc
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-05-05 18:41:02 +0900

    whoops change types of JSON

--- appattest-circuits/lib/src/lib.rs
+++ appattest-circuits/lib/src/lib.rs
@@ -2,19 +2,19 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AttestationStatement {
-    pub x5c: Vec<String>,
-    pub receipt: String,
+    pub x5c: [Vec<u8>;2], // array of intermediate and leaf certificates.
+    pub receipt: Vec<u8>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AttestationObject {
     pub fmt: String,
     pub attStmt: AttestationStatement,
-    pub authData: String,
+    pub authData: Vec<u8>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AssertionObject {
-    pub signature: String,
-    pub authenticatorData: String,
+    pub signature: Vec<u8>,
+    pub authenticatorData: Vec<u8>,
 }
