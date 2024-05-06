e08c193e7113f9dde9de5bb56ebe393c3447cd16
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-05-06 10:38:16 +0900

    decode

--- appattest-circuits/program/src/assertion.rs
+++ appattest-circuits/program/src/assertion.rs
@@ -1,29 +1,39 @@
 use sha2::Digest;
 use sha2::Sha256;
-use ring::signature;
-use ring::signature::UnparsedPublicKey;
+// use ring::signature;
+// use ring::signature::UnparsedPublicKey;
 use lib::AssertionObject;
+use p256::{ecdsa::{VerifyingKey, signature::Verifier, Signature}, PublicKey};
+use hex;
 
-pub fn validate_assertion(assertion: AssertionObject, challenge: String, public_key_bytes: Vec<u8>) -> bool {
+pub fn validate_assertion(assertion: AssertionObject, client_data: String, public_key_uncompressed_hex: String) -> bool {
 
     // 1. sha256 hash the clientData
     let mut hasher = Sha256::new();
-    hasher.update(challenge);
+    hasher.update(client_data);
     let client_data_hash = hasher.finalize();
-    // println!("\nCLIENT DATA HASH: {:?}", client_data_hash);
 
     // 2. Create nonce.
     let mut hasher = Sha256::new();
-    let mut raw_nonce: Vec<u8> = assertion.authenticator_data.as_bytes().to_vec();
+    let mut raw_nonce: Vec<u8> = assertion.authenticator_data;
     raw_nonce.extend(&client_data_hash);
     
     hasher.update(raw_nonce);
     let nonce = hasher.finalize();
 
     // 3. Verify signature over nonce.
-    let public_key = UnparsedPublicKey::new(&signature::ECDSA_P256_SHA256_ASN1, public_key_bytes);
-    let verification = public_key.verify(&nonce, assertion.signature.as_ref());
+    let public_key_uncompressed = hex::decode(public_key_uncompressed_hex).expect("decoding error");
+    let public_key = PublicKey::from_sec1_bytes(&public_key_uncompressed).expect("import error");
+    let verifying_key = VerifyingKey::from(&public_key);
 
+    let signature = Signature::from_der(&assertion.signature).expect("deserializing error");
+
+    println!("\nNONCE: {:?}", nonce);
+    println!("\nVERIFYING KEY: {:?}", verifying_key);
+    println!("\nSIGNATURE: {:?}", signature);
+    println!("");
+
+    let verification = verifying_key.verify(&nonce, &signature);
     let verified = match verification {
         Ok(_) => {
             println!("Signature verified!");
--- appattest-circuits/program/src/decode.rs
+++ appattest-circuits/program/src/decode.rs
@@ -1,8 +1,9 @@
 use base64_url::decode;
 use cbor::{Decoder};
 use rustc_serialize::json::{ToJson};
-use lib::{AttestationObject, AssertionObject};
+use lib::{AttestationObject, AssertionObject, AssertionStr};
 use serde_json;
+use base64::{engine::general_purpose::STANDARD, Engine as _};
 
 // Decode base64 string into attestation object.
 pub fn decode_attestation(encoded: &str) -> Result<AttestationObject, serde_json::Error> {
@@ -17,11 +18,21 @@ pub fn decode_attestation(encoded: &str) -> Result<AttestationObject, serde_json
 
 // Decode base64 string into assertion object.
 pub fn decode_assertion(encoded: &str) -> Result<AssertionObject, serde_json::Error> {
-    let _bytes = base64_url::decode(encoded).unwrap();
+    // CBOR decode.
     let decoded = decode(encoded).unwrap();
     let mut d = Decoder::from_bytes(decoded);
     let cbor = d.items().next().unwrap().unwrap();
     let json_str = cbor.to_json();
-    let assertion: AssertionObject = serde_json::from_str(json_str.to_string().as_str())?;
+    let assertion_str: AssertionStr = serde_json::from_str(json_str.to_string().as_str())?;
+    
+    // Base64 Decode.
+    let signature_bytes = STANDARD.decode(&assertion_str.signature).expect("signature decoding error");
+    let authenticator_data_bytes = STANDARD.decode(&assertion_str.authenticator_data).expect("auth data decoding error");
+
+    // Return AssertionObject.
+    let assertion = AssertionObject {
+        signature: signature_bytes,
+        authenticator_data: authenticator_data_bytes,
+    };
     Ok(assertion)
 }
