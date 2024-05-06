564b0bd626155a037e1c58fc008b05b220112a27
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-05-06 13:34:18 +0900

    wip

--- appattest-circuits/lib/src/lib.rs
+++ appattest-circuits/lib/src/lib.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct AttestationStatement {
-    pub x5c: Vec<String>, // array of intermediate and leaf certificates.
+    pub x5c: Vec<String>, 
     pub alg: i32,
 }
 
@@ -19,7 +19,7 @@ pub struct AttestationObject {
 #[serde(rename_all = "camelCase")]
 pub struct AssertionObject {
     pub signature: Vec<u8>,
-    pub authenticator_data: Vec<u8>,
+    pub authenticator_data: AuthenticatorData,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -29,17 +29,17 @@ pub struct AssertionStr {
     pub authenticator_data: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AttestedCredentialData {
     pub aaguid: Vec<u8>,
     pub credential_id: Vec<u8>,
     pub credential_public_key: Vec<u8>
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AuthenticatorData {
     pub rp_id: Vec<u8>,
-    pub flags: u8, // TODO: might have to change
+    pub flags: u8, 
     pub counter: u32,
     pub att_data: AttestedCredentialData,
     pub ext_data: Vec<u8>,
