8ef256c95b97f08e127e8fa8ca5ab892cf6b9297
Sofianel5 <sofianef1110@gmail.com>
2024-05-07 13:46:11 +0900

    changed-decoder

--- appattest-circuits/program/src/decode.rs
+++ appattest-circuits/program/src/decode.rs
@@ -1,17 +1,43 @@
 use base64_url::decode;
-use lib::{AttestationObject, AssertionObject, AuthenticatorData, ClientData};
+use lib::{AttestationObject, AttestationStatement, AssertionObject, AuthenticatorData, ClientData};
 use serde_json;
-use base64::{engine::general_purpose::URL_SAFE, Engine as _};
+use base64::{engine::general_purpose::{URL_SAFE, STANDARD}, Engine as _};
 
 use serde_cbor::{Value, from_slice};
 
 pub fn decode_attestation(encoded: String) -> Result<AttestationObject, serde_json::Error> {
-    let decoded = URL_SAFE.decode(&encoded.as_bytes()).expect("decoding error");
+    let decoded = base64_url::decode(&encoded).unwrap();
     let cbor: Value = from_slice(&decoded).expect("decoding error");
     let json_str = serde_json::to_string(&cbor).expect("decoding error");
-    let attestation: AttestationObject = serde_json::from_str(&json_str).expect("decoding error");
+    let attestation: serde_json::Value = serde_json::from_str(&json_str).expect("decoding error");
     
-    Ok(attestation)
+    Ok(AttestationObject {
+        fmt: attestation["fmt"].as_str().unwrap().to_string(),
+        att_stmt: AttestationStatement {
+            x5c: attestation["attStmt"]["x5c"].as_array().unwrap().iter().map(|x| {
+                match x {
+                    serde_json::Value::Array(a) => {
+                        let bytes: Vec<u8> = a.iter().map(|v| {
+                            match v {
+                                serde_json::Value::Number(i) => i.as_u64().unwrap() as u8,
+                                _ => panic!("Unexpected value in x5c"),
+                            }
+                        }).collect();
+                        base64::encode(bytes)
+                    },
+                    _ => panic!("Unexpected value in x5c"),
+                }
+            }).collect(),
+        },
+        auth_data: base64::encode(
+            attestation["authData"].as_array().unwrap().iter().map(|x| {
+                match x {
+                    serde_json::Value::Number(i) => i.as_u64().unwrap() as u8,
+                    _ => panic!("Unexpected value in authData"),
+                }
+            }
+        ).collect::<Vec<u8>>())
+    })
 }
 
 // Decode base64 string into assertion object.
@@ -38,7 +64,7 @@ pub fn decode_assertion_auth_data(s: Vec<u8>) -> Result<AuthenticatorData, serde
 
 // Base64 decode.
 pub fn decode_base64_to_bytes(encoded: &String) -> Vec<u8> {
-    let decoded = decode(&encoded).unwrap();
+    let decoded = STANDARD.decode(&encoded).unwrap();
     decoded
 }
 
