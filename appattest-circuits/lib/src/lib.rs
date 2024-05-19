6a2c4cd0db4b0aa65415c8db3a5ddedbee7fc8c2
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-05-19 12:34:39 +0900

    cargo changes;

--- appattest-circuits/lib/src/lib.rs
+++ appattest-circuits/lib/src/lib.rs
@@ -3,12 +3,11 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct AttestationStatement {
-    pub x5c: Vec<String>, 
-    // pub alg: i32,
+    pub x5c: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde( rename_all = "camelCase")]
+#[serde(rename_all = "camelCase")]
 pub struct AttestationObject {
     pub fmt: String,
     pub att_stmt: AttestationStatement,
@@ -26,20 +25,19 @@ pub struct AssertionObject {
 pub struct AttestedCredentialData {
     pub aaguid: Vec<u8>,
     pub credential_id: Vec<u8>,
-    pub credential_public_key: Vec<u8>
+    pub credential_public_key: Vec<u8>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AuthenticatorData {
     pub rp_id: Vec<u8>, // 32 bytes
-    pub flags: u8, // 1 byte 
+    pub flags: u8,      // 1 byte
     pub counter: u32,
-    pub aaguid: Option<Vec<u8>>
-    // pub att_data: AttestedCredentialData,
-    // pub ext_data: Vec<u8>,
+    pub aaguid: Option<Vec<u8>>, // pub att_data: AttestedCredentialData,
+                                 // pub ext_data: Vec<u8>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ClientData {
-    pub challenge: String
+    pub challenge: String,
 }
